8c0e644dfd7dd054e777aa7f8d3c8566f9b001e7
Lachlan Hardy <lachlan@lachstock.com.au>
2013-02-05 07:33:04 -0800

    first cut
    a rough cut to show a working version. I'll add more details and docs
    later. Probably.

---
+++ grunt.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var path = require("path"),
+    jade = require("jade");
+
+module.exports = function (grunt) {
+
+    grunt.registerTask('default', 'A simple build from Jade to HTML.', function () {
+
+        function writeFile(src, dest) {
+            var jadeFile = grunt.file.read(src),
+                htmlFile = jade.compile(jadeFile, { filename: src, pretty: true })();
+
+            grunt.file.write(dest, htmlFile);
+            grunt.log.writeln("Main HTML file created at \"" + dest + "\".");
+        };
+
+        writeFile("default.jade", "default.html");
+
+    });
+
+};
